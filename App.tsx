c83b3d740eb4205d6995ee9e1e601ffb0f3a07f6
Eran Avichzer <eranavichzer@Erans-Mac-mini.lan>
2022-10-24 20:36:00 +0300

    added react import

--- App.tsx
+++ App.tsx
@@ -1,45 +1,6 @@
+import React from "react"
 import { AppMain } from "./src/screens/AppMain"
 
 export default function App() {
 	return <AppMain />
 }
-
-// <SafeAreaView style={styles.container}>
-//    <Text>Hello Eran Avichzer</Text>
-//    <Text>Good Day!</Text>
-
-//    <Text onPress={havdlePress}>PRESS HERE</Text>
-//    <Text numberOfLines={1}>Fucker Fucker Fucker Fucker Fucker Fucker Fucker Fucker Fucker Fucker</Text>
-
-//    <Image source={require('./assets/icon.png')} style={{ width: 250, height: 250 }} resizeMode={'cover'} />
-
-//    <TouchableWithoutFeedback>
-//       <Image source={{ uri: 'https://picsum.photos/id/1/200/300' }} style={{ width: 100, height: 100 }} resizeMode={'cover'} />
-//    </TouchableWithoutFeedback>
-
-//    <TouchableOpacity>
-//       <Image source={{ uri: 'https://picsum.photos/id/2/200/300' }} style={{ width: 100, height: 100 }} resizeMode={'cover'} />
-//    </TouchableOpacity>
-
-//    <Button title='Click Me' color={'black'} onPress={() => console.log('Pressed')} />
-
-//    <Button title='Alert' color={'black'} onPress={() => alert('Pressed')} />
-//    <Button title='Alert with btn' color={'black'} onPress={() => Alert.alert('my title', 'my msg', [{ text: 'yes' }, { text: 'no' }])} />
-//    <Button title='Alert prompt' color={'black'} onPress={() => Alert.prompt('my title', 'my msg', (text) => console.log(text))} />
-
-//    <StatusBar style="auto" />
-
-//    <View style={{ flex: 1, backgroundColor: 'gold', }}>
-//    </View>
-
-//    <View style={{ flex: 1, backgroundColor: 'tomato', }}>
-//    </View>
-// </SafeAreaView>
-
-// console.log('App executed');
-// console.log(useDimensions())
-// console.log(useDeviceOrientation())
-
-// const havdlePress = () => {
-//    console.log('Pressed');
-// }
